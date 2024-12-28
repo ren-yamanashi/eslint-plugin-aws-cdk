4b2ee7c94669e97155c847b9c8d747b9373bdc4c
ren-yamanashi <pinokionigg@icloud.com>
2024-12-28 14:31:17 +0900

    chore: support declarationMap

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,32 @@
-npx tsc -p tsconfig.build.json && \
-find dist -mindepth 1 ! -name 'index.d.ts' ! -name 'index.d.ts.map' -exec rm -rf {} + && \
-mkdir -p dist/types && \
-mv dist/index.d.ts dist/types/index.d.ts && \
-mv dist/index.d.ts.map dist/types/index.d.ts.map && \
-pkgroll --tsconfig=tsconfig.build.json && \
-mv dist/types/index.d.ts dist/index.d.ts && \
-mv dist/types/index.d.ts.map dist/index.d.ts.map && \
-rm -rf dist/types
+#!/bin/bash
+
+# NOTE: This library uses `pkgroll` to generate d.ts, but using `d.ts` created by tsc because `pkgroll` does not support `declarationMap`.
+
+# Terminate script on error
+set -e
+
+DIST_DIR="dist"
+TYPES_DIR="${DIST_DIR}/types"
+
+# build types
+npx tsc -p tsconfig.build.json || exit 1
+
+# Delete unnecessary files (other than those related to index.d.ts)
+find ${DIST_DIR} -mindepth 1 ! -name 'index.d.ts' ! -name 'index.d.ts.map' -exec rm -rf {} +
+
+# move type definition files
+mkdir -p ${TYPES_DIR}
+mv ${DIST_DIR}/index.d.ts ${TYPES_DIR}/
+mv ${DIST_DIR}/index.d.ts.map ${TYPES_DIR}/
+
+# build
+pkgroll --tsconfig=tsconfig.build.json || exit 1
+
+# restore type definition files
+mv ${TYPES_DIR}/index.d.ts ${DIST_DIR}/
+mv ${TYPES_DIR}/index.d.ts.map ${DIST_DIR}/
+
+# delete temporary directory
+rm -rf ${TYPES_DIR}
+
+echo "Build completed successfully!"
