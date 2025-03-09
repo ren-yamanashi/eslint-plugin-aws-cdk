e4d56afc50f63472947c451fe20b146bf20f371d
ren-yamanashi <pinokionigg@icloud.com>
2025-03-09 17:36:57 +0900

    feat: update

--- src/rules/construct-constructor-property.ts
+++ src/rules/construct-constructor-property.ts
@@ -7,8 +7,7 @@ import {
 
 import { isConstructType } from "../utils/typeCheck";
 
-type Options = [];
-type MessageIds = "invalidConstructorProperty";
+type Context = TSESLint.RuleContext<"invalidConstructorProperty", []>;
 
 /**
  * Enforces that constructors of classes extending Construct have the property names 'scope, id' or 'scope, id, props'
@@ -16,65 +15,56 @@ type MessageIds = "invalidConstructorProperty";
  * @returns An object containing the AST visitor functions
  * @see {@link https://eslint-cdk-plugin.dev/rules/construct-constructor-property} - Documentation
  */
-export const constructConstructorProperty = ESLintUtils.RuleCreator.withoutDocs<
-  Options,
-  MessageIds
->({
-  meta: {
-    type: "problem",
-    docs: {
-      description:
-        "Enforces that constructors of classes extending Construct have the property names 'scope, id' or 'scope, id, props'",
-    },
-    messages: {
-      invalidConstructorProperty:
-        "Constructor of a class extending Construct must have the property names 'scope, id' or 'scope, id, props'",
-    },
-    schema: [],
-  },
-  defaultOptions: [],
-  create(context) {
-    const parserServices = ESLintUtils.getParserServices(context);
-
-    return {
-      ClassDeclaration(node) {
-        // Skip if the class doesn't have a superclass
-        if (!node.superClass) return;
-
-        // Get the type of the superclass
-        const superClassType = parserServices.getTypeAtLocation(
-          node.superClass
-        );
-
-        // Skip if the class doesn't extend Construct
-        if (!isConstructType(superClassType)) return;
-
-        // Find the constructor method
-        const constructor = node.body.body.find(
-          (member): member is TSESTree.MethodDefinition =>
-            member.type === AST_NODE_TYPES.MethodDefinition &&
-            member.kind === "constructor"
-        );
-
-        // Skip if there's no constructor
-        if (!constructor) return;
-
-        validateConstructorProperty(constructor, context);
+export const constructConstructorProperty = ESLintUtils.RuleCreator.withoutDocs(
+  {
+    meta: {
+      type: "problem",
+      docs: {
+        description:
+          "Enforces that constructors of classes extending Construct have the property names 'scope, id' or 'scope, id, props'",
       },
-    };
-  },
-});
+      messages: {
+        invalidConstructorProperty:
+          "Constructor of a class extending Construct must have the property names 'scope, id' or 'scope, id, props'",
+      },
+      schema: [],
+    },
+    defaultOptions: [],
+    create(context) {
+      const parserServices = ESLintUtils.getParserServices(context);
+
+      return {
+        ClassDeclaration(node) {
+          const type = parserServices.getTypeAtLocation(node);
+          if (!isConstructType(type)) return;
+
+          // NOTE: Find the constructor method
+          const constructor = node.body.body.find(
+            (member): member is TSESTree.MethodDefinition =>
+              member.type === AST_NODE_TYPES.MethodDefinition &&
+              member.kind === "constructor"
+          );
+
+          // NOTE: Skip if there's no constructor
+          if (!constructor) return;
+
+          validateConstructorProperty(constructor, context);
+        },
+      };
+    },
+  }
+);
 
 /**
  * Validates that the constructor has the property names "scope, id" or "scope, id, props"
  */
 const validateConstructorProperty = (
   constructor: TSESTree.MethodDefinition,
-  context: TSESLint.RuleContext<MessageIds, Options>
+  context: Context
 ): void => {
   const params = constructor.value.params;
 
-  // Check if the constructor has at least 2 parameters
+  // NOTE: Check if the constructor has at least 2 parameters
   if (params.length < 2) {
     context.report({
       node: constructor,
@@ -83,7 +73,7 @@ const validateConstructorProperty = (
     return;
   }
 
-  // Check if the first parameter is named "scope"
+  // NOTE: Check if the first parameter is named "scope"
   const firstParam = params[0];
   if (
     firstParam.type === AST_NODE_TYPES.Identifier &&
@@ -96,7 +86,7 @@ const validateConstructorProperty = (
     return;
   }
 
-  // Check if the second parameter is named "id"
+  // NOTE: Check if the second parameter is named "id"
   const secondParam = params[1];
   if (
     secondParam.type === AST_NODE_TYPES.Identifier &&
@@ -109,18 +99,19 @@ const validateConstructorProperty = (
     return;
   }
 
-  // If there's a third parameter, check if it's named "props"
-  if (params.length >= 3) {
-    const thirdParam = params[2];
-    if (
-      thirdParam.type === AST_NODE_TYPES.Identifier &&
-      thirdParam.name !== "props"
-    ) {
-      context.report({
-        node: constructor,
-        messageId: "invalidConstructorProperty",
-      });
-      return;
-    }
+  // NOTE: If there's no third parameter, return
+  if (params.length < 3) return;
+
+  // NOTE: Check if the third parameter is named "props"
+  const thirdParam = params[2];
+  if (
+    thirdParam.type === AST_NODE_TYPES.Identifier &&
+    thirdParam.name !== "props"
+  ) {
+    context.report({
+      node: constructor,
+      messageId: "invalidConstructorProperty",
+    });
+    return;
   }
 };
