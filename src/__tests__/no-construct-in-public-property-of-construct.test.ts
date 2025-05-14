7c6f760842990544ae80082dd27320c206e71b07
ren-yamanashi <pinokionigg@icloud.com>
2025-05-14 20:25:04 +0900

    test: update

--- src/__tests__/no-construct-in-public-property-of-construct.test.ts
+++ src/__tests__/no-construct-in-public-property-of-construct.test.ts
@@ -20,118 +20,119 @@ ruleTester.run(
       // WHEN: field type is primitive
       {
         code: `
-      class Construct {}
-      class TestClass extends Construct {
-        public test: string;
-      }
-      `,
+          class Construct {}
+          class TestClass extends Construct {
+            public test: string;
+          }
+        `,
       },
       // WHEN: field type is interface
       {
         code: `
-      class Construct {}
-      interface ITest {
-        value: string;
-      }
-      class TestClass extends Construct {
-        public test: ITest;
-      }
-      `,
+          class Construct {}
+          interface ITest {
+            value: string;
+          }
+          class TestClass extends Construct {
+            public test: ITest;
+          }
+        `,
       },
       // WHEN: field type is type alias
       {
         code: `
-      class Construct {}
-      type TestType = {
-        value: string;
-      };
-      class TestClass extends Construct {
-        public test: TestType;
-      }
-      `,
+          class Construct {}
+          type TestType = {
+            value: string;
+          };
+          class TestClass extends Construct {
+            public test: TestType;
+          }
+        `,
       },
       // WHEN: field is private
       {
         code: `
-      class Construct {}
-      class DependencyClass {}
-      class TestClass extends Construct {
-        private test: DependencyClass;
-      }
-      `,
+          class Construct {}
+          class DependencyClass extends Construct {}
+          class TestClass extends Construct {
+            private test: DependencyClass;
+          }
+        `,
       },
       // WHEN: field has no type annotation
       {
         code: `
-      class Construct {}
-      class TestClass extends Construct {
-        public test;
-      }
-      `,
+          class Construct {}
+          class TestClass extends Construct {
+            public test;
+          }
+        `,
       },
       // WHEN: field is protected
       {
         code: `
-      class Construct {}
-      class DependencyClass {}
-      class TestClass extends Construct {
-        protected test: DependencyClass;
-      }
-      `,
+          class Construct {}
+          class DependencyClass extends Construct {}
+          class TestClass extends Construct {
+            protected test: DependencyClass;
+          }
+        `,
       },
       // WHEN: super class is not a construct
       {
         code: `
-      class DependencyClass {}
-      class TestClass {
-        public test: DependencyClass;
-      }
-      `,
+          class Construct {}
+          class DependencyClass extends Construct {}
+          class TestClass {
+            public test: DependencyClass;
+          }
+        `,
       },
     ],
     invalid: [
-      // WHEN: public field uses class type
+      // WHEN: public field uses Construct type
       {
         code: `
-      class Construct {}
-      class DependencyClass {}
-      class TestClass extends Construct {
-        public test: DependencyClass;
-      }
-      `,
+          class Construct {}
+          class DependencyClass extends Construct {}
+          class TestClass extends Construct {
+            public test: DependencyClass;
+          }
+        `,
         errors: [{ messageId: "noConstructInPublicPropertyOfConstruct" }],
       },
-      // WHEN: implicitly public field uses class type
+      // WHEN: implicitly public field uses Construct type
       {
         code: `
-      class Construct {}
-      class DependencyClass {}
-      class TestClass extends Construct {
-        test: DependencyClass;
-      }
-      `,
+          class Construct {}
+          class DependencyClass extends Construct {}
+          class TestClass extends Construct {
+            test: DependencyClass;
+          }
+        `,
         errors: [{ messageId: "noConstructInPublicPropertyOfConstruct" }],
       },
       // WHEN: readonly public field uses class type
       {
         code: `
-      class Construct {}
-      class DependencyClass {}
-      class TestClass extends Construct {
-        public readonly test: DependencyClass;
-      }
-      `,
+          class Construct {}
+          class DependencyClass extends Construct {}
+          class TestClass extends Construct {
+            public readonly test: DependencyClass;
+          }
+        `,
         errors: [{ messageId: "noConstructInPublicPropertyOfConstruct" }],
       },
-      // WHEN: constructor parameter property uses class type
+      // WHEN: constructor parameter property uses Construct type
       {
         code: `
-      class Construct {}
-      class DependencyClass {}
-      class TestClass extends Construct {
-        constructor(public test: DependencyClass) {}
-      }
-      `,
+          class Construct {}
+          class DependencyClass extends Construct {}
+          class TestClass extends Construct {
+            constructor(public test: DependencyClass) {}
+          }
+        `,
         errors: [{ messageId: "noConstructInPublicPropertyOfConstruct" }],
       },
     ],
