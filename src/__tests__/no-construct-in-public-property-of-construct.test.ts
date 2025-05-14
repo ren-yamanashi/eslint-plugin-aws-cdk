6ec5f67026e8f37924c815bf69ae21918bdb5e0f
Ren Yamanashi <96362223+ren-yamanashi@users.noreply.github.com>
2025-05-14 21:06:08 +0900

    feat: "no-public-class-fields" rule to "no-construct-in-public-property-of-construct" (#152)
    * feat: "no-public-class-fields" rule to "no-construct-in-public-property-of-construct"
    
    * docs: update
    
    * test: update

---
+++ src/__tests__/no-construct-in-public-property-of-construct.test.ts
@@ -0,0 +1,140 @@
+import { RuleTester } from "@typescript-eslint/rule-tester";
+
+import { noConstructInPublicPropertyOfConstruct } from "../rules/no-construct-in-public-property-of-construct";
+
+const ruleTester = new RuleTester({
+  languageOptions: {
+    parserOptions: {
+      projectService: {
+        allowDefaultProject: ["*.ts*"],
+      },
+    },
+  },
+});
+
+ruleTester.run(
+  "no-construct-in-public-property-of-construct",
+  noConstructInPublicPropertyOfConstruct,
+  {
+    valid: [
+      // WHEN: field type is primitive
+      {
+        code: `
+          class Construct {}
+          class TestClass extends Construct {
+            public test: string;
+          }
+        `,
+      },
+      // WHEN: field type is interface
+      {
+        code: `
+          class Construct {}
+          interface ITest {
+            value: string;
+          }
+          class TestClass extends Construct {
+            public test: ITest;
+          }
+        `,
+      },
+      // WHEN: field type is type alias
+      {
+        code: `
+          class Construct {}
+          type TestType = {
+            value: string;
+          };
+          class TestClass extends Construct {
+            public test: TestType;
+          }
+        `,
+      },
+      // WHEN: field is private
+      {
+        code: `
+          class Construct {}
+          class DependencyClass extends Construct {}
+          class TestClass extends Construct {
+            private test: DependencyClass;
+          }
+        `,
+      },
+      // WHEN: field has no type annotation
+      {
+        code: `
+          class Construct {}
+          class TestClass extends Construct {
+            public test;
+          }
+        `,
+      },
+      // WHEN: field is protected
+      {
+        code: `
+          class Construct {}
+          class DependencyClass extends Construct {}
+          class TestClass extends Construct {
+            protected test: DependencyClass;
+          }
+        `,
+      },
+      // WHEN: super class is not a construct
+      {
+        code: `
+          class Construct {}
+          class DependencyClass extends Construct {}
+          class TestClass {
+            public test: DependencyClass;
+          }
+        `,
+      },
+    ],
+    invalid: [
+      // WHEN: public field uses Construct type
+      {
+        code: `
+          class Construct {}
+          class DependencyClass extends Construct {}
+          class TestClass extends Construct {
+            public test: DependencyClass;
+          }
+        `,
+        errors: [{ messageId: "noConstructInPublicPropertyOfConstruct" }],
+      },
+      // WHEN: implicitly public field uses Construct type
+      {
+        code: `
+          class Construct {}
+          class DependencyClass extends Construct {}
+          class TestClass extends Construct {
+            test: DependencyClass;
+          }
+        `,
+        errors: [{ messageId: "noConstructInPublicPropertyOfConstruct" }],
+      },
+      // WHEN: readonly public field uses class type
+      {
+        code: `
+          class Construct {}
+          class DependencyClass extends Construct {}
+          class TestClass extends Construct {
+            public readonly test: DependencyClass;
+          }
+        `,
+        errors: [{ messageId: "noConstructInPublicPropertyOfConstruct" }],
+      },
+      // WHEN: constructor parameter property uses Construct type
+      {
+        code: `
+          class Construct {}
+          class DependencyClass extends Construct {}
+          class TestClass extends Construct {
+            constructor(public test: DependencyClass) {}
+          }
+        `,
+        errors: [{ messageId: "noConstructInPublicPropertyOfConstruct" }],
+      },
+    ],
+  }
+);
--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,12 @@ import { name, version } from "../package.json";
 
 import { constructConstructorProperty } from "./rules/construct-constructor-property";
 import { noConstructInInterface } from "./rules/no-construct-in-interface";
+import { noConstructInPublicPropertyOfConstruct } from "./rules/no-construct-in-public-property-of-construct";
 import { noConstructStackSuffix } from "./rules/no-construct-stack-suffix";
 import { noImportPrivate } from "./rules/no-import-private";
 import { noMutablePropertyOfPropsInterface } from "./rules/no-mutable-property-of-props-interface";
 import { noMutablePublicPropertyOfConstruct } from "./rules/no-mutable-public-property-of-construct";
 import { noParentNameConstructIdMatch } from "./rules/no-parent-name-construct-id-match";
-import { noPublicClassFields } from "./rules/no-public-class-fields";
 import { noVariableConstructId } from "./rules/no-variable-construct-id";
 import { pascalCaseConstructId } from "./rules/pascal-case-construct-id";
 import { propsNameConvention } from "./rules/props-name-convention";
@@ -18,20 +18,21 @@ import { requirePassingThis } from "./rules/require-passing-this";
 import { requirePropsDefaultDoc } from "./rules/require-props-default-doc";
 
 const rules = {
-  "no-construct-in-interface": noConstructInInterface,
-  "no-construct-stack-suffix": noConstructStackSuffix,
-  "no-parent-name-construct-id-match": noParentNameConstructIdMatch,
-  "no-public-class-fields": noPublicClassFields,
-  "pascal-case-construct-id": pascalCaseConstructId,
-  "require-passing-this": requirePassingThis,
-  "no-variable-construct-id": noVariableConstructId,
-  "no-mutable-public-property-of-construct": noMutablePublicPropertyOfConstruct,
-  "no-mutable-property-of-props-interface": noMutablePropertyOfPropsInterface,
   "construct-constructor-property": constructConstructorProperty,
-  "require-jsdoc": requireJSDoc,
-  "require-props-default-doc": requirePropsDefaultDoc,
-  "props-name-convention": propsNameConvention,
+  "no-construct-in-interface": noConstructInInterface,
+  "no-construct-in-public-property-of-construct":
+    noConstructInPublicPropertyOfConstruct,
+  "no-construct-stack-suffix": noConstructStackSuffix,
   "no-import-private": noImportPrivate,
+  "no-mutable-property-of-props-interface": noMutablePropertyOfPropsInterface,
+  "no-mutable-public-property-of-construct": noMutablePublicPropertyOfConstruct,
+  "no-parent-name-construct-id-match": noParentNameConstructIdMatch,
+  "no-variable-construct-id": noVariableConstructId,
+  "pascal-case-construct-id": pascalCaseConstructId,
+  "props-name-convention": propsNameConvention,
+  "require-jsdoc": requireJSDoc,
+  "require-passing-this": requirePassingThis,
+  "require-props-default-doc": requirePropsDefaultDoc,
 };
 
 const cdkPlugin = {
@@ -55,39 +56,39 @@ const createFlatConfig = (rules: Record<string, unknown>) => {
 };
 
 const recommended = createFlatConfig({
+  "cdk/construct-constructor-property": "error",
   "cdk/no-construct-in-interface": "error",
+  "cdk/no-construct-in-public-property-of-construct": "error",
   "cdk/no-construct-stack-suffix": "error",
+  "cdk/no-mutable-property-of-props-interface": "warn",
+  "cdk/no-mutable-public-property-of-construct": "warn",
   "cdk/no-parent-name-construct-id-match": [
     "error",
     { disallowContainingParentName: false },
   ],
-  "cdk/no-public-class-fields": "error",
+  "cdk/no-variable-construct-id": "error",
   "cdk/pascal-case-construct-id": "error",
   "cdk/require-passing-this": ["error", { allowNonThisAndDisallowScope: true }],
-  "cdk/no-variable-construct-id": "error",
-  "cdk/no-mutable-public-property-of-construct": "warn",
-  "cdk/no-mutable-property-of-props-interface": "warn",
-  "cdk/construct-constructor-property": "error",
 });
 
 const strict = createFlatConfig({
+  "cdk/construct-constructor-property": "error",
   "cdk/no-construct-in-interface": "error",
+  "cdk/no-construct-in-public-property-of-construct": "error",
   "cdk/no-construct-stack-suffix": "error",
+  "cdk/no-import-private": "error",
+  "cdk/no-mutable-property-of-props-interface": "error",
+  "cdk/no-mutable-public-property-of-construct": "error",
   "cdk/no-parent-name-construct-id-match": [
     "error",
     { disallowContainingParentName: true },
   ],
-  "cdk/no-public-class-fields": "error",
-  "cdk/pascal-case-construct-id": "error",
-  "cdk/require-passing-this": "error",
   "cdk/no-variable-construct-id": "error",
-  "cdk/no-mutable-public-property-of-construct": "error",
-  "cdk/no-mutable-property-of-props-interface": "error",
-  "cdk/construct-constructor-property": "error",
-  "cdk/require-jsdoc": "error",
-  "cdk/require-props-default-doc": "error",
+  "cdk/pascal-case-construct-id": "error",
   "cdk/props-name-convention": "error",
-  "cdk/no-import-private": "error",
+  "cdk/require-jsdoc": "error",
+  "cdk/require-passing-this": "error",
+  "cdk/require-props-default-doc": "error",
 });
 
 const configs = {
