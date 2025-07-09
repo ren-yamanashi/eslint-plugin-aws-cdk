31448e7f85d6374918d4a1ddc009a8380c74ba80
ren-yamanashi <pinokionigg@icloud.com>
2025-07-09 22:42:16 +0900

    fix: Type errors occur when adding @ts-check comments in eslint.config.js

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import tsParser from "@typescript-eslint/parser";
+import { FlatConfig } from "@typescript-eslint/utils/ts-eslint";
 
 import { name, version } from "../package.json";
 
@@ -40,7 +41,13 @@ const cdkPlugin = {
   rules,
 };
 
-const createFlatConfig = (rules: Record<string, unknown>) => {
+const createFlatConfig = (
+  rules: FlatConfig.Rules
+): {
+  languageOptions: FlatConfig.LanguageOptions;
+  plugins: FlatConfig.Plugins;
+  rules: FlatConfig.Rules;
+} => {
   return {
     languageOptions: {
       parser: tsParser,
@@ -100,7 +107,7 @@ export { configs, rules };
 
 export interface EslintCdkPlugin {
   rules: typeof rules;
-  configs: typeof configs;
+  configs: Readonly<Record<string, FlatConfig.Config>>;
 }
 
 const eslintCdkPlugin: EslintCdkPlugin = {
