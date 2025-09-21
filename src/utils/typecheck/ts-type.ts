5b3a65df64df5f2b0c8640e7c2942496268a4d78
ren-yamanashi <pinokionigg@icloud.com>
2025-09-21 13:03:42 +0900

    refactor: update

--- src/utils/typecheck/ts-type.ts
+++ src/utils/typecheck/ts-type.ts
@@ -5,6 +5,7 @@ import { SYMBOL_FLAGS } from "../../constants/tsInternalFlags";
 // NOTE: In order not to make it dependent on the typescript library, it defines its own unions.
 //       Therefore, the type information structures do not match.
 /* eslint-disable @typescript-eslint/no-unsafe-enum-comparison */
+
 const getSymbol = (type: Type): Symbol | undefined => {
   return type.getSymbol?.() ?? type.symbol;
 };
