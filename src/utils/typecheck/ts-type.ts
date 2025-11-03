9835577765a02bc9ab0b2a1633e2f15820b44a9a
ren-yamanashi <pinokionigg@icloud.com>
2025-11-03 15:34:10 +0900

    fix: support typescript v5.9

--- src/utils/typecheck/ts-type.ts
+++ src/utils/typecheck/ts-type.ts
@@ -1,17 +1,11 @@
-import { Symbol, Type } from "typescript";
-
-import { SYMBOL_FLAGS } from "../../constants/tsInternalFlags";
-
-// NOTE: In order not to make it dependent on the typescript library, it defines its own unions.
-//       Therefore, the type information structures do not match.
-/* eslint-disable @typescript-eslint/no-unsafe-enum-comparison */
+import { Symbol, SymbolFlags, Type } from "typescript";
 
 const getSymbol = (type: Type): Symbol | undefined => {
   return type.getSymbol?.() ?? type.symbol;
 };
 
 export const isClassType = (type: Type): boolean => {
-  return getSymbol(type)?.flags === SYMBOL_FLAGS.CLASS;
+  return getSymbol(type)?.flags === SymbolFlags.Class;
 };
 
 export const isArrayType = (type: Type): boolean => {
