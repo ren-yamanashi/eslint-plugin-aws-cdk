96e2a99c03011391f3f1eee4cceda368d8074d14
Ren Yamanashi <96362223+ren-yamanashi@users.noreply.github.com>
2025-11-07 10:46:32 +0900

    chore: use tsdown (#301)
    * chore: use tsdown
    
    * chore: rm unused file
    
    * chore: update tsconfig.json

---
+++ tsdown.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "tsdown";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  outDir: "dist",
+  clean: true,
+  format: ["esm", "cjs"],
+  dts: true,
+  outputOptions: { exports: "named" },
+  treeshake: true,
+  fixedExtension: true,
+});
