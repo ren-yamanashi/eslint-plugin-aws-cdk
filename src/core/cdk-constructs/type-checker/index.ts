b7fc85c0d04271afcab8fb246825bb2299d41c99
ren-yamanashi <pinokionigg@icloud.com>
2025-12-02 23:52:24 +0900

    refactor: remove unnecessary blank line in type-checker index

--- src/core/cdk-constructs/type-checker/index.ts
+++ src/core/cdk-constructs/type-checker/index.ts
@@ -2,4 +2,3 @@ export { isConstructType } from "./is-construct";
 export { isConstructOrStackType } from "./is-construct-or-stack";
 export { isResourceType } from "./is-resource";
 export { isResourceWithReadonlyInterface } from "./is-resource-with-readonly-interface";
-
